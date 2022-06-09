4aa21804e304c804cbdcd25e1b2dc9231e2b58e5
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-06-08 22:58:17 -0500

    add the filter for search the characters for name and the button for add to the favorites the selected character

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,8 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer, useMemo } from "react";
 import "../styles/Characters.css";
+
+const initialState = {
+  favorites: [],
+};
+
+const favoriteReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_FAVORITE":
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload],
+      };
+    default:
+      return state;
+  }
+};
 
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
+  const [search, setSearch] = useState("");
+  const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+
+  const handleClick = (favorite) => {
+    dispatch({ type: "ADD_TO_FAVORITE", payload: favorite });
+  };
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  // const filterCharacters = characters.filter((character) => {
+  //   return character.name.toLowerCase().includes(search.toLowerCase());
+  // });
+
+  const filterCharacters = useMemo(
+    () =>
+      characters.filter((character) => {
+        return character.name.toLowerCase().includes(search.toLowerCase());
+      }),
+    [characters, search]
+  );
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character/")
@@ -11,9 +49,17 @@ const Characters = () => {
   }, []);
 
   return (
-    <div class="container">
-      {characters.map((character) => (
-        <div className="card">
+    <div className="container">
+      {favorites.favorites.map((favorite) => (
+        <li key={favorite.id}>{favorite.name}</li>
+      ))}
+
+      <div className="search">
+        <input type="text" value={search} onChange={handleSearch} />
+      </div>
+
+      {filterCharacters.map((character) => (
+        <div className="card" key={character.id}>
           <figure className="image">
             <img src={character.image} alt="" />
           </figure>
@@ -22,6 +68,9 @@ const Characters = () => {
           <p>{character.species}</p>
           <p>{character.gender}</p>
           <p>{character.location.name}</p>
+          <button type="button" onClick={() => handleClick(character)}>
+            Add Favorites
+          </button>
         </div>
       ))}
       <h2>{}</h2>
